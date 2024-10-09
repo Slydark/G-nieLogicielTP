890032591ded1d5f06e52b1890e568c58037fb1e
Slydark <gundamwing_1986@hotmail.com>
2024-10-09 14:25:23 +0200

    Création d'une focntion filtre

---
+++ TP2/main.py
@@ -0,0 +1,9 @@
+# ==========================
+# Séance II - Python, numpy, matplotlib
+# ==========================
+# ==========================
+# Problème II.1 : Passe-bas à réponse impulsionnelle exponentielle - 1/3
+# ==========================
+# Vu que le programme qu'on va crée pour nous habituer à utiliser les fonctions 
+# dans un autre fichier que la main.py puis l'appeler dans le main.py
+# je saute cette partie pour la faire dans le fichier filtre.py
